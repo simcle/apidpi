2b7d0a26c301a2c274013ca5ef9d481151dbb67e
simcle <nendisofiandy84@gmail.com>
2023-04-05 14:40:42 +0700

    updage

--- src/routers/update.js
+++ src/routers/update.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 
 const Invoices = require('../models/invoice');
 router.put('/', (req, res) => {
-    let id = mongoose.Types.ObjectId('642d151f5d2380b5fa8b8212')
-    console.log(id);
+    let id = mongoose.Types.ObjectId('642cf060cce6c4b1bc622cad')
     Invoices.findById(id)
     .then( result => {
         result.amountDue = result.grandTotal
